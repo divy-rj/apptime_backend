33cf81d7712d5822a159579a733faee1d9a8b8d2
Divyashu Raj <1905825@kiit.ac.in>
2021-08-25 21:58:49 +0530

    user schema 2

--- models/user.js
+++ models/user.js
@@ -1,7 +1,29 @@
 const mongoose=require('mongoose');
 const joi=require('joi');
 const bcrypt=require('bcrypt');
+const appschema=mongoose.Schema({
+    appname:{
+        type:String,
+    },
+    usage_time:{
+        type:Number,
+    }
+})
+
+const appusagelistschema=mongoose.Schema({
+    usage_date:{
+        type:Date,
+    },
+    usages:{
+       type:[appschema],
+    }
+
+})
+const appusagelist=mongoose.model('User',userschema);
 const userschema=mongoose.Schema({
+    parentalacess:{
+        type:Boolean,
+    },
     name:{
         type:String,
         required:true
@@ -17,7 +39,20 @@ const userschema=mongoose.Schema({
         required:true,
         minLength: 6,
         maxLength: 300
-    }
+    },
+    usage:[{
+        usage_date:{
+            type:Date,
+        },
+        usages:[{
+            appname:{
+                type:String,
+            },
+            usage_time:{
+                type:Number,
+            },
+        }]
+    }],
 });
 const User=mongoose.model('User',userschema);
 function validateuser(user){
